b073388e3bec3bc25a0019c233e79380665d8f2e
Shrivardhan <chshrivardhan@gmail.com>
2026-06-17 16:55:15 +0530

    Log4j Module To Log Spring-Security

--- open-itsm-parent/application/src/main/java/com/openitsm/application/controller/IncidentController.java
+++ open-itsm-parent/application/src/main/java/com/openitsm/application/controller/IncidentController.java
@@ -2,6 +2,8 @@ package com.openitsm.application.controller;
 
 import com.openitsm.incident.domain.Incident;
 import com.openitsm.incident.service.IncidentService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +13,17 @@ import java.util.List;
 public class IncidentController {
 
     private final IncidentService service;
-
+    private static final Logger logger = LogManager.getLogger(IncidentController.class);
     public IncidentController(IncidentService service) {
         this.service = service;
     }
 
     @GetMapping("/api/incidents")
     public List<Incident> findAll() {
+        logger.info("Info - Retrieving all incidents");
+        logger.error("Error - Retrieving all incidents");
+        logger.debug("Debug - Retrieving all incidents");
+        logger.fatal("Fatal - Retrieving all incidents");
         return service.findAll();
     }
 }
