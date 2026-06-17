02fe94c6fbe4df7574f06fbc497efec4f501e585
Shrivardhan <chshrivardhan@gmail.com>
2026-06-17 15:47:23 +0530

    Removed BCrypt

--- open-itsm-parent/application/src/main/java/com/openitsm/application/Application.java
+++ open-itsm-parent/application/src/main/java/com/openitsm/application/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication(scanBasePackages = "com.openitsm")
 @EnableJpaRepositories(basePackages = "com.openitsm")
