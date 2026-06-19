4af56126e6ca81a7d96d4557b93f2d625165197f
Shrivardhan <chshrivardhan@gmail.com>
2026-06-19 11:17:19 +0530

    Migrated JDBCTemplate -> JPA

--- open-itsm-parent/service-user/src/main/java/com/openitsm/user/repository/UserRepository.java
+++ open-itsm-parent/service-user/src/main/java/com/openitsm/user/repository/UserRepository.java
@@ -1,59 +1,13 @@
 package com.openitsm.user.repository;
 
 import java.util.Optional;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.openitsm.user.model.AppUser;
 
 @Repository
-public class UserRepository {
-    private static final Logger log = LogManager.getLogger(UserRepository.class);
-    private final JdbcTemplate jdbcTemplate;
+public interface UserRepository extends JpaRepository<AppUser, Long> {
 
-    public UserRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
+    Optional<AppUser> findByUsername(String username);
 
-    //Find User By Username
-    public Optional<AppUser> findByUsername(String username) {
-        log.debug("Looking up user in database: {}", username);
-
-        String sql = """
-                SELECT USER_ID,
-                       USERNAME,
-                       PASSWORD,
-                       ROLE,
-                       ENABLED
-                FROM USERS
-                WHERE USERNAME = ?
-                """;
-
-        return jdbcTemplate.query(sql, rs -> {
-            if (rs.next()) {
-                log.debug("User record found for {}", username);
-                AppUser user = new AppUser();
-                user.setId(rs.getLong("USER_ID"));
-                user.setUsername(rs.getString("USERNAME"));
-                user.setPassword(rs.getString("PASSWORD"));
-                user.setRole(rs.getString("ROLE"));
-                user.setEnabled(rs.getString("ENABLED"));
-                return Optional.of(user);
-            }
-            log.warn("No user record found for {}", username);
-            return Optional.empty();
-        }, username);
-    }
-
-    // INSERT USER (create-user flow)
-    public void save(AppUser user) {
-        String sql = """
-                INSERT INTO USERS
-                (USER_ID, USERNAME, PASSWORD, ROLE, ENABLED)
-                VALUES (USER_SEQ.NEXTVAL, ?, ?, ?, ?)
-                """;
-        jdbcTemplate.update(sql, user.getUsername(), user.getPassword(), user.getRole(), user.getEnabled());
-    }
 }
